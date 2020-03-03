c288e86fda9e5b5dd6666c932c672fed0bf2270b
Gekif <dzulfikar.maulana@gmail.com>
2020-03-03 21:14:24 +0700

    04. Accepting Input: 0402_Working With PUT and DELETE Requests

--- app/Http/Controllers/v1/FlightController.php
+++ app/Http/Controllers/v1/FlightController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\v1;
 
 use App\Services\v1\FlightsService;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -80,7 +81,20 @@ class FlightController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $flight = $this->flights->updateFlight($request, $id);
+
+            return response()->json($flight, 200);
+        }
+
+        catch (ModelNotFoundException $exception) {
+            throw $exception;
+        }
+
+        catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 500);
+        }
+
     }
 
     /**
@@ -91,6 +105,16 @@ class FlightController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            $flight = $this->flights->deleteFlight($id);
+
+            return response()->make('', 204);
+        }
+        catch (ModelNotFoundException $exception) {
+            throw $exception;
+        }
+        catch (Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 500);
+        }
     }
 }
